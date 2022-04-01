fdec9be117788183fdf3002c86c25b6722b89a8a
EngJao89 <eng_joao@outlook.com>
2022-04-01 09:39:53 -0300

    Close 04:"Implementa CORS."

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
+const cors = require('cors');
+
 module.exports = mongoose.connect('mongodb+srv://<admin>:<password>@cluster0.bvzl8.mongodb.net/database?retryWrites=true&w=majority',{
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -11,6 +13,7 @@ module.exports = mongoose.connect('mongodb+srv://<admin>:<password>@cluster0.bvz
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3200);
